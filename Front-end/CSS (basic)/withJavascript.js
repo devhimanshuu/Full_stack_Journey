e6e9cc2d8c27f19e3d685420f2756e4661711824
devhimanshuu <devhimanshuu@gmail.com>
2023-10-10 00:06:07 +0530

    add withJavascript

---
+++ Front-end/CSS (basic)/withJavascript.js
@@ -0,0 +1,5 @@
+/* It's possible to add, remove or change CSS property values with JavaScript through an element's style property. */
+
+var el = document.getElementById("element");
+el.style.opacity = 0.5;
+el.style.fontFamily = "sans-serif";
