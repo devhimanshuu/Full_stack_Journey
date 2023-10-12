3352ee96f0bc595599a5f20d4dd0b357c33ed97a
devhimanshuu <devhimanshuu@gmail.com>
2023-10-13 00:56:40 +0530

    add typeOfvar

---
+++ Front-end/Javascript(Basic)/typeOfvar.js
@@ -0,0 +1,11 @@
+var myInteger = 12; // 32-bit number (from -2,147,483,648 to 2,147,483,647)
+var myLong = 9310141419482; // 64-bit number (from -9,223,372,036,854,775,808 to 9,223,372,854,775,807)
+var myFloat = 5.5; // 32-bit floating-point number (decimal)
+var myDouble = 9310141419482.22; // 64-bit floating-point number
+var myBoolean = true; // 1-bit true/false (0 or 1)
+var myBoolean2 = false;
+var myNotANumber = NaN;
+var NaN_Example = 0 / 0; // NaN: Division by Zero is not possible
+var notDefined; // undefined: we didn't define it to anything yet
+window.alert(aRandomVariable); // undefined
+var myNull = null; // null
