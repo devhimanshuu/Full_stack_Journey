dd9f0d10e474da48f433c157bd0c722811a73fa9
devhimanshuu <devhimanshuu@gmail.com>
2023-11-18 11:19:22 +0530

    data

---
+++ Back-End/Node.js/data.js
@@ -0,0 +1,10 @@
+const data = [
+  { name: "Himanshu Gupta", email: "devhimanshuu@gmail.com" },
+  { name: "Sumit (battery)", email: "devhimanshuu@gmail.com" },
+  { name: "Neelesh (chutiya)", email: "devhimanshuu@gmail.com" },
+  { name: "Abhay (saanp)", email: "devhimanshuu@gmail.com" },
+  { name: "Aman (khabda)", email: "devhimanshuu@gmail.com" },
+  { name: "Roopsi (chipakali)", email: "devhimanshuu@gmail.com" },
+];
+
+module.exports = data;
