91469384a3ef740de7412173d8f9b8f9006097f4
devhimanshuu <devhimanshuu@gmail.com>
2023-11-29 00:17:03 +0530

    insertDocInNode

---
+++ Back-End/MongoDB/insertDocInNode.js
@@ -0,0 +1,26 @@
+const dbname = "bank";
+const collection_name = "accounts";
+
+const accountsCollection = client.db(dbname).collection(collection_name);
+
+const sampleAccount = {
+  account_holder: "Linus Torvalds",
+  account_id: "MDB829001337",
+  account_type: "checking",
+  balance: 50352434,
+};
+
+const main = async () => {
+  try {
+    await connectToDatabase();
+    // insertOne method is used here to insert the sampleAccount document
+    let result = await accountsCollection.insertOne(sampleAccount);
+    console.log(`Inserted document: ${result.insertedId}`);
+  } catch (err) {
+    console.error(`Error inserting document: ${err}`);
+  } finally {
+    await client.close();
+  }
+};
+
+main();
