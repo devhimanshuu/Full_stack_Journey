4521ce3560866e35055737e3cc8a0353710b8b26
devhimanshuu <devhimanshuu@gmail.com>
2023-12-03 00:10:53 +0530

    mongoAggwithNode

---
+++ Back-End/MongoDB/mongoAggwithNode.js
@@ -0,0 +1,34 @@
+const client = new MongoClient(uri);
+const dbname = "bank";
+const collection_name = "accounts";
+const accountsCollection = client.db(dbname).collection(collection_name);
+const pipeline = [
+  // Stage 1: match the accounts with a balance greater than $1,000
+  { $match: { balance: { $lt: 1000 } } },
+  // Stage 2: Calculate average balance and total balance
+  {
+    $group: {
+      _id: "$account_type",
+      total_balance: { $sum: "$balance" },
+      avg_balance: { $avg: "$balance" },
+    },
+  },
+];
+const main = async () => {
+  try {
+    await client.connect();
+    console.log(
+      `Connected to the database 🌍. \nFull connection string: ${safeURI}`
+    );
+    let result = await accountsCollection.aggregate(pipeline);
+    for await (const doc of result) {
+      console.log(doc);
+    }
+  } catch (err) {
+    console.error(`Error connecting to the database: ${err}`);
+  } finally {
+    await client.close();
+  }
+};
+
+main();
