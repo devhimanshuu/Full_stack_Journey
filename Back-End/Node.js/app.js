b8e954390257bb12382344a18645e6fe5c8369f4
devhimanshuu <devhimanshuu@gmail.com>
2023-12-20 17:21:46 +0530

    app

---
+++ Back-End/Node.js/app.js
@@ -0,0 +1,13 @@
+//input from command line
+
+const fs = require("fs");
+const input = process.argv;
+
+if (input[2] == "add") {
+  fs.writeFileSync(input[3], input[4]);
+} else if (input[2] == "remove") {
+  fs.unlinkSync(input[3]);
+} else {
+  console.log("invalid input");
+}
+fs.writeFileSync(input[2], input[3]);
