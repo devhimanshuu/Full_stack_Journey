c0fcdfc2cdfb040f66ed97487699c26db820a9db
devhimanshuu <devhimanshuu@gmail.com>
2023-12-27 22:45:58 +0530

    objectkeys

---
+++ Front-end/Javascript(Basic)/objectkeys.js
@@ -0,0 +1,8 @@
+// Object.keys(obj) returns an array of a given object's keys.
+var obj = {
+  a: "hello",
+  b: "this is",
+  c: "javascript!",
+};
+var keys = Object.keys(obj);
+console.log(keys); // ["a", "b", "c"]
