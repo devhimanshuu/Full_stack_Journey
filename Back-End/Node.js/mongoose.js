169ef51e441280fe17330ea37267d8366c5ff989
devhimanshuu <devhimanshuu@gmail.com>
2024-01-08 03:32:27 +0530

    mongoose

---
+++ Back-End/Node.js/mongoose.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const fs = require("fs");
+const mongoose = require("mongoose");
+const app = express();
+const port = 5000;
+
+mongoose
+  .connect("url")
+  .then(() => console.log("mongoDB connected"))
+  .catch((err) => console.log("Mongo Error", err));
+// schema
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  jobTitle: {
+    type: String,
+  },
+  gender: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("user", userSchema);
+app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  console.log("hello from middleare-1");
+  return res.json({ msg: "hello from middleware-1" });
+  next();
+});
+app.use((req, res, next) => {
+  console.log("hello from middleare-2");
+  return res.json({ msg: "hello from middleware-2" });
+  next();
+});
+
+//routers
+app.get("/users", (req, res) => {
+  const html = `<ul>
+            ${users.map((users) => `<li>${users.first_name}</li>`).json}
+    </ul>`;
+});
+app.listen(5000);
