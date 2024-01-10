29db14d8d55e18ab294e324b5324d849d0aea2ea
devhimanshuu <devhimanshuu@gmail.com>
2024-01-10 23:50:00 +0530

    staticRouter

---
+++ Back-End/PracticeProject/urlShortner/routes/staticRouter.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const allUrls = await URL.find({});
+  return res.render("home", { urls: allUrls });
+});
+
+router.get("/signup", (req, res) => {
+  return res.render("signup");
+});
+module.exports = router;
