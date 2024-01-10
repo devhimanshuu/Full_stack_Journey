0c1c934cbf3193c805e079bd48253bfb53405afd
devhimanshuu <devhimanshuu@gmail.com>
2024-01-10 23:46:04 +0530

    controller-url

--- Back-End/PracticeProject/urlShortner/controllers/url.js
+++ Back-End/PracticeProject/urlShortner/controllers/url.js
@@ -4,13 +4,20 @@ const URL = require("../models/url");
 async function handleGenerateNewShortURL(req, res) {
   const body = req.body;
   if (!body.url) return res.status(400).json({ error: "url is required" });
-  const shortID = shortid(8);
+  const shortID = shortid();
   await URL.create({
     shortId: shortID,
     redirectURL: body.url,
     visitedHistory: [],
   });
-  return res.json({ id: shortID });
+  return res.render("home", { id: shortID });
 }
-
-module.exports = { handleGenerateNewShortURL };
+async function handleGetAnalytics(req, res) {
+  const shortId = req.params.shortId;
+  const result = await URL.findOne({ shortId });
+  return res.json({
+    totalClicks: result.visitHistory.length,
+    analytics: result.visitHistory,
+  });
+}
+module.exports = { handleGenerateNewShortURL, handleGetAnalytics };
