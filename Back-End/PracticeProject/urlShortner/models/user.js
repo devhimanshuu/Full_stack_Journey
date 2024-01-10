181fefc5af43f14bf4a73db3e4d9b0174c978ce4
devhimanshuu <devhimanshuu@gmail.com>
2024-01-10 23:49:42 +0530

    staticRouter

---
+++ Back-End/PracticeProject/urlShortner/models/user.js
@@ -0,0 +1,28 @@
+//for authentication
+const express = require("express");
+const router = express.router();
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const user = model("user", userSchema);
+
+module.exports = user;
