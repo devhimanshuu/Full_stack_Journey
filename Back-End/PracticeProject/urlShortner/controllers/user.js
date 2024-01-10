17c5e8755bfa6ab29373fceb003380d8b65f4e67
devhimanshuu <devhimanshuu@gmail.com>
2024-01-10 23:49:24 +0530

    user

---
+++ Back-End/PracticeProject/urlShortner/controllers/user.js
@@ -0,0 +1,12 @@
+const User = require("../models/user").default;
+
+async function handleUserSignUp(req, res) {
+  const { name, email, password } = req.body;
+  await User.create({
+    name,
+    email,
+    password,
+  });
+  return res.render("home");
+}
+module.exports = { handleUserSignUp };
