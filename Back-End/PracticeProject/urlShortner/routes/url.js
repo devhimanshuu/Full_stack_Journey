9a629892a1bd91bc3e4fca5b804ea6c35ad3c72e
devhimanshuu <devhimanshuu@gmail.com>
2024-01-10 23:50:13 +0530

    modified

--- Back-End/PracticeProject/urlShortner/routes/url.js
+++ Back-End/PracticeProject/urlShortner/routes/url.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const { handleGenerateNewShortURL } = require("../controllers/url");
+const {
+  handleGenerateNewShortURL,
+  handleGetAnalytics,
+} = require("../controllers/url");
 const router = express.Router();
 
 router.post("/", handleGenerateNewShortURL);
 
+router.get("/analytics/:shortId", handleGetAnalytics);
 module.exports = router;
