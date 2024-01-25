4ff432a01143ba255d94a9bf3b068a3acbfd0742
devhimanshuu <devhimanshuu@gmail.com>
2024-01-25 22:11:03 +0530

    settingCookies

---
+++ Back-End/Node.js/settingCookies.js
@@ -0,0 +1,18 @@
+// The following is an example for setting and reading cookies using the cookie-parser module:
+var express = require("express");
+var cookieParser = require("cookie-parser"); // module for parsing cookies
+var app = express();
+app.use(cookieParser());
+app.get("/setcookie", function (req, res) {
+  // setting cookies
+  res.cookie("username", "john doe", { maxAge: 900000, httpOnly: true });
+  return res.send("Cookie has been set");
+});
+app.get("/getcookie", function (req, res) {
+  var username = req.cookies["username"];
+  if (username) {
+    return res.send(username);
+  }
+  return res.send("No cookie found");
+});
+app.listen(3000);
