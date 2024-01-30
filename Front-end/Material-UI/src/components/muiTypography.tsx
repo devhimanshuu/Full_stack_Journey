8553e1a9d06309cd49ef62fdb2080123a4f64d28
devhimanshuu <devhimanshuu@gmail.com>
2024-01-30 20:06:36 +0530

    muiTypography

---
+++ Front-end/Material-UI/src/components/muiTypography.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+export const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant="h1">Heading h1</Typography>
+      <Typography variant="h2">Heading h2</Typography>
+      <Typography variant="h3">Heading h3</Typography>
+      <Typography variant="h4">Heading h4</Typography>
+      <Typography variant="h5">Heading h5</Typography>
+      <Typography variant="h6">Heading h6</Typography>
+      <Typography variant="subtitle1">Subtitle 1</Typography>
+      <Typography variant="subtitle2">Subtitle 2</Typography>
+      <Typography variant="body1">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia saepe
+        tempore natus nemo assumenda possimus, illo, consectetur ipsum ratione
+        hic qui aut! Atque totam sint, vitae deleniti architecto blanditiis
+        porro.
+      </Typography>
+    </div>
+  );
+};
