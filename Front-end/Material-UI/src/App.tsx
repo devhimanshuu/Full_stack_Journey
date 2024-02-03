f00c495f68e21c7505d65fe7ad4742d47603f38b
devhimanshuu <devhimanshuu@gmail.com>
2024-02-04 02:05:10 +0530

    ,,

---
+++ Front-end/Material-UI/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+import MuiButton from "./components/muiButton";
+function App() {
+  return (
+    <div className="App">
+      <MuiButton />
+    </div>
+  );
+}
+
+export default App;
