5aabdd8d8212940ad6f6cbb896eda410f089bbf2
devhimanshuu <devhimanshuu@gmail.com>
2024-02-04 02:03:09 +0530

    modified

--- Front-end/Material-UI/src/components/muiTypography.tsx
+++ Front-end/Material-UI/src/components/muiTypography.tsx
@@ -7,7 +7,9 @@ export const MuiTypography = () => {
       <Typography variant="h1">Heading h1</Typography>
       <Typography variant="h2">Heading h2</Typography>
       <Typography variant="h3">Heading h3</Typography>
-      <Typography variant="h4">Heading h4</Typography>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Heading h4
+      </Typography>
       <Typography variant="h5">Heading h5</Typography>
       <Typography variant="h6">Heading h6</Typography>
       <Typography variant="subtitle1">Subtitle 1</Typography>
