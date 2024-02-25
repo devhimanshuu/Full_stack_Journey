20f5ad672d064aadd5bad1646f52b43495587c3d
devhimanshuu <devhimanshuu@gmail.com>
2024-02-25 12:41:49 +0530

    muiNavbar

---
+++ Front-end/Material-UI/src/components/muiNavbar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Stack,
+  Button,
+} from "@mui/material";
+import CatchingPokemon from "@mui/icons-material/CatchingPokemon";
+export const MuiNavbar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton size="large" edge="start" color="inherit" aria-label="logo">
+          <CatchingPokemon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Pokemon
+        </Typography>
+        <Stack direction="row" spacing={2}>
+          <Button color="inherit">Features</Button>
+          <Button color="inherit">Pricing</Button>
+          <Button color="inherit">About</Button>
+          <Button color="inherit">Login</Button>
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
+};
