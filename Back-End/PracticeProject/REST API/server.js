17d5f6bead3543a14cabe6f85e6d8cc6b520398f
devhimanshuu <devhimanshuu@gmail.com>
2024-03-13 02:12:26 +0530

    modified

--- Back-End/PracticeProject/REST API/server.js
+++ Back-End/PracticeProject/REST API/server.js
@@ -5,5 +5,7 @@ const app = express();
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/subusers", { useNewUrlParser: true });
 const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("error", () => console.error("Connected to Database"));
 
-app.listen(3000, () => console.log("hey this server is made by himanshu "));
+app.listen(3000, () => console.log("Hey this server is made by himanshu "));
