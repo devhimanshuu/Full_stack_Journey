e8799c17c468f5cc0575eee5e8f1e9a0d54e0092
devhimanshuu <devhimanshuu@gmail.com>
2024-06-10 23:51:59 +0530

    Variable

---
+++ Back-End/TypeScript/01_Intro/Variable.ts
@@ -0,0 +1,13 @@
+let greetings: string = "Hello Typescript";
+
+greetings.toLowerCase();
+console.log(greetings);
+
+//number
+let userId: number = 3333.5;
+userId.toFixed();
+
+//boolean
+let isLoggedIn: boolean = false;
+
+export {};
