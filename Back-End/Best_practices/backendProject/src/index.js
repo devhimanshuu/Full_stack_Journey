9fdfeed8cb35c7199a09875780b466bc2a6766d6
devhimanshuu <devhimanshuu@gmail.com>
2024-06-15 19:16:00 +0530

    constants

---
+++ Back-End/Best_practices/backendProject/src/index.js
@@ -0,0 +1,41 @@
+import dotenv from "dotenv";
+
+import mongoose from "mongoose";
+
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed!!!", err);
+  });
+
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    app.on("error", (error) => {  //listener on express
+      console.log("ERROR:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("ERROR:", error);
+    throw err;
+  }
+})();
+*/
