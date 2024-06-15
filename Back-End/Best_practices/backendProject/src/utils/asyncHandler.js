0ccf988da6aa008101745ffd265b7cfe9e0a5ac5
devhimanshuu <devhimanshuu@gmail.com>
2024-06-15 23:11:43 +0530

    all config

---
+++ Back-End/Best_practices/backendProject/src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
+
+export { asyncHandler };
