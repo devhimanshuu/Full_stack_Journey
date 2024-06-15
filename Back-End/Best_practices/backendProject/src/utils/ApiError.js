4c1bf1c7b03d813c3d9d7fe74cc149bbf69c351c
devhimanshuu <devhimanshuu@gmail.com>
2024-06-15 23:11:23 +0530

    Api

---
+++ Back-End/Best_practices/backendProject/src/utils/ApiError.js
@@ -0,0 +1,22 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "something went wrong",
+    errors = [],
+    statck = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+
+    if (statck) {
+      this.stack = statck;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+export { ApiError };
