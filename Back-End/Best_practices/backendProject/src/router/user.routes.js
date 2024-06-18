0e848319c79a64e8a39ee42355b391595222dc6e
devhimanshuu <devhimanshuu@gmail.com>
2024-06-18 17:47:42 +0530

    modified

--- Back-End/Best_practices/backendProject/src/router/user.routes.js
+++ Back-End/Best_practices/backendProject/src/router/user.routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
-
+import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  registerUser
+);
 // router.route("/login").post(login);
 
 export default router;
